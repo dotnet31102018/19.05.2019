7395b8770741bd27476d2ebbd54f9c6d2dcb7f22
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-05-19 18:10:00 +0300

    Add files via upload

---
+++ DelegateStart/Delegate1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate1
+{
+    class Program
+    {
+
+        // method void which accpets String
+        public delegate void SignatureGetStringReturnVoid(string message);
+
+        static SignatureGetStringReturnVoid method1;
+
+        // void which accepts String
+        static void MyPrinter(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Imposter(int x)
+        {
+
+        }
+
+        static void Execute1(SignatureGetStringReturnVoid func)
+        {
+            if (func != null)
+            {
+                func.Invoke("Hello Again!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Employee e1 = new Employee();
+            // Emplopyee e = e1;
+            method1 = MyPrinter;
+
+            //method1 = Imposter; -- wrong!
+
+            method1.Invoke("Hello World!");
+            Execute1(method1);
+        }
+    }
+}
