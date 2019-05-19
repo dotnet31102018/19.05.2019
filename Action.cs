c4f204794ebbbde20f95dde8c54b39c70092c96d
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-05-19 21:25:23 +0300

    Create Action.cs

---
+++ Action.cs
@@ -0,0 +1,6 @@
+        // delegate (2 lines booooooooo)
+        //public delegate void MyActionVideo(Video v);
+        //public MyActionVideo TheAfterFunciton { get; set; }
+
+        // Action (1 line !!!)
+        public Action<Video> TheAfterFunciton { get; set; }
